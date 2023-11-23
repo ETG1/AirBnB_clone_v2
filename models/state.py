3d5485377b2742dc3718f5abda13009d3fd83420
ETG1 <elijahkazungu22@gmail.com>
2023-11-22 17:46:12 -0800

    Updated

--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    
+    # For DBStorage
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+    # For FileStorage
+    else:
+        @property
+        def cities(self):
+            ''' Getter attribute for cities in FileStorage '''
+            from models import storage
+            cities_list = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
+
